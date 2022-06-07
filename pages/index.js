bc617acad9ee33869c4f3501206803aa1d172dbb
mapsam <matthews.sam@gmail.com>
2022-06-06 17:03:00 -0700

    emojify the weather

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,55 @@ export async function getServerSideProps() {
   return { props: { json } };
 }
 
+function getEmoji(item, raw) {
+  let val = '☁';
+  switch(item.weather[0].main) {
+    case 'Clouds':
+      if (item.clouds <= 25) {
+        val = '🌤'
+      } else if (item.clouds > 25 && item.clouds <= 50) {
+        val = '⛅'
+      } else if (item.clouds > 50 && item.clouds <= 75) {
+        val = '🌥'
+      } else {
+        val = '☁';
+      }
+      break;
+    case 'Thunderstorm':
+      val = '🌩';
+      break;
+    case 'Drizzle':
+      val = '🌧';
+      break;
+    case 'Rain':
+      val = '🌧';
+      break;
+    case 'Snow':
+      val = '❄';
+      break;
+    case 'Mist':
+      val = '☁';
+      break;
+    case 'Haze':
+      val = '☁';
+      break;
+    case 'Fog':
+      val = '🌫';
+      break;
+    case 'Clear':
+      val = '🌙	';
+      break;
+    default:
+      break;
+  }
+
+  if (!raw) {
+    return (<span className="emoji">{val}</span>);
+  } else {
+    return val;
+  }
+}
+
 export default function Index({ json }) {
   if (json.cod) {
     return (
@@ -24,8 +73,13 @@ export default function Index({ json }) {
 
   return (
     <div>
+      <Head>
+        <title>Weattle</title>
+        <link rel="icon" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${getEmoji(json.current, true)}</text></svg>`}></link>
+      </Head>
+
       <div className='full'>
-        <h1>Weattle. ☁️</h1>
+        <h1>Weattle. {getEmoji(json.current)}</h1>
         <p className='italics'>
           Weather in Seattle.&nbsp;
           {format(utcToZonedTime(new Date(json.current.dt * 1000), json.timezone), 'yyyy-MM-dd HH:mm (z)', { timeZone: json.timezone })}
@@ -35,7 +89,7 @@ export default function Index({ json }) {
 
       <div className='container'>
         <h3>Current</h3>
-        <p>{Math.round(json.current.temp)}&deg;F ({json.current.weather.map(w => w.main).join(', ')})</p>
+        <p>{Math.round(json.current.temp)}&deg;F {getEmoji(json.current)}</p>
       </div>
 
       <div className='container'>
@@ -49,7 +103,7 @@ export default function Index({ json }) {
           return (
             <p>
               {dtpre}&nbsp;{hr}:&nbsp;
-              {Math.round(h.temp)}&deg;F ({h.weather.map(w => w.main).join(', ')})
+              {Math.round(h.temp)}&deg;F {getEmoji(h)}
             </p>
           );
         })}
@@ -64,7 +118,7 @@ export default function Index({ json }) {
           return (
             <p>
               {dt}:&nbsp;
-              {Math.round(day.temp.day)}&deg;F [{Math.round(day.temp.min)}/{Math.round(day.temp.max)}] ({day.weather.map(w => w.main).join(', ')})
+              {Math.round(day.temp.day)}&deg;F [{Math.round(day.temp.min)}/{Math.round(day.temp.max)}] {getEmoji(day)}
             </p>
           );
         })}
